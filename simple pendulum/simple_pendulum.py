d83f14a98577ce2588273867b28a4b1ebf52f961
Kartik17 <32801164+Kartik17@users.noreply.github.com>
2018-09-06 22:55:42 +0530

    Update simple_pendulum.py

--- simple pendulum/simple_pendulum.py
+++ simple pendulum/simple_pendulum.py
@@ -1,28 +1,27 @@
 import numpy as np
 g = 9.81
-c = 0.05
+c = 0.005
 l =10.
+m= 1.
 
 delta_time = 0.1
 time_start = 0.0
 time_end = 10.0
 
-previous_theta = (1*np.pi)/180.
-previous_angvel = 0.
+theta_list = [(1*np.pi)/180.]
+ang_vel_list = [0.]
 
-theta_list = []
-ang_vel_list = []
 
 for i in range(500):
-    ang_vel = -(g/l)*previous_theta*delta_time -c*previous_angvel + previous_angvel
-    new_theta = previous_theta + previous_angvel*delta_time
+    ang_vel_der = -(g/l)*theta_list[-1] -(c/m*l*l)*ang_vel_list[-1]
 
-    theta_list.append(new_theta)
-    ang_vel_list.append(ang_vel)
+    ang_vel_current = (ang_vel_der)*delta_time + ang_vel_list[-1]
+    theta_current = theta_list[-1] + ang_vel_list[-1]*delta_time
 
-    previous_angvel = ang_vel
-    previous_theta = new_theta
+    theta_list.append(theta_current)
+    ang_vel_list.append(ang_vel_current)
+
 
 import matplotlib.pyplot as plt
-plt.plot(range(len(theta_list)),theta_list)
+plt.plot(range(len(ang_vel_list)),theta_list)
 plt.show()
