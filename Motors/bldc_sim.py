1f1d929bfeba8b748cee9645f59d9b459911f0ca
Kartik17 <32801164+Kartik17@users.noreply.github.com>
2018-09-19 06:56:15 +0530

    Updates 1.2
    Included Current variation through the inverter.
    Checked for No-load current, Stall Torque, No Load RPM.
    Gears need to be added.
    Refer: Paper - webfiles.portal.chalmers.se/et/MSc/BaldurssonStefanMSc.pdf

--- Motors/bldc_sim.py
+++ Motors/bldc_sim.py
@@ -3,14 +3,15 @@ import numpy as np
 # Define Constants
 V = 6. #Volts
 R = 12.5 # Ohms
-kb = 0.5*1.05e-3 #Volts/sec
+kb = 1.05e-3
+ke = kb #Volts/sec
 kt = 1.05e-3 #Nm/A
 b = 1.38e-8 #
 I = 0.005e-7
 L = 0.091e-3
 p = 1
 T_load = 0.
-delta_t = L/(R*2)
+delta_t = L/(R*20)
 total_time = delta_t*1000
 
 
@@ -28,7 +29,7 @@ def back_emf_func(theta):
     elif theta >= 5*np.pi/3. and theta < 12*np.pi/6.:
         return -1 + (6/np.pi)*(theta - 5*np.pi/3.)
 
-V_seq = {'1':[V/2.,-V/2.,0],'2':[V/2.,0.,-V/2],'3':[0.,V/2.,-V/2.],'4':[-V/2.,V/2.,0],'5':[-V/2.,0.,V/2.],'6':[0, -V/2., V/2.]}
+V_seq = {'1':[V,-V/2.,0],'2':[V/2.,0.,-V/2],'3':[0.,V/2.,-V/2.],'4':[-V/2.,V/2.,0],'5':[-V/2.,0.,V/2.],'6':[0, -V/2., V/2.]}
 i_phase = [[0.],[0.],[0.]]
 
 w = [0]
@@ -36,61 +37,254 @@ back_emf_a = [0.0]
 theta_m = [0.]
 theta_e = [0.]
 theta_e_2pi_list = [0.]
-import math
 torque_list =[0.]
 w_rpm = [0.]
 
-
+import math
 print(kt*(V/R), V/kb)
 
-for i in range(1,25000):
+def back_emf_phase(theta,w_inst,ke):
+    return (ke/2)*w_inst*back_emf_func(theta),(ke/2)*w_inst*back_emf_func(theta-2*np.pi/3.),(ke/2)*w_inst*back_emf_func(theta-4*np.pi/3.)
+
+for i in range(1,200000):
     #Electrical Angle = P*Mechanical Angle
     theta_e_2pi = (theta_e[i-1])%(2*math.pi)
     theta_m_2pi = (theta_m[i-1])%(2*math.pi)
     #print(theta_m[i-1])
-    if theta_e_2pi >= 0.0 and theta_e_2pi < np.pi/3.:
-        i_phase[0].append((delta_t/L)*(V_seq['1'][0] - R*i_phase[0][i-1] - kb*back_emf_func(theta_e_2pi)*w[i-1]) + i_phase[0][i-1])
-        i_phase[1].append((delta_t/L)*(V_seq['1'][1] - R*i_phase[1][i-1] - kb*back_emf_func(theta_e_2pi - 2*np.pi/3.)*w[i-1]) + i_phase[1][i-1])
-        i_phase[2].append(0.)
-        back_emf_a.append(kb*back_emf_func(theta_e_2pi)*w[i-1])
 
-    elif theta_e_2pi >= np.pi/3. and theta_e_2pi < 2*np.pi/3.:
-        i_phase[0].append((delta_t/L)*(V_seq['2'][0] - R*i_phase[0][i-1] -kb*back_emf_func(theta_e_2pi)*w[i-1]) + i_phase[0][i-1])
-        i_phase[1].append(0.0)
-        i_phase[2].append((delta_t/L)*(V_seq['2'][2] - R*i_phase[2][i-1] -kb*back_emf_func(theta_e_2pi - 4*np.pi/3.)*w[i-1]) + i_phase[2][i-1])
-        back_emf_a.append(kb*back_emf_func(theta_e_2pi)*w[i-1])
+################### 0 -60 #########################
+    if theta_e_2pi >= 0.0 and theta_e_2pi < np.pi/3. and i_phase[1][i-1] <=0 and i_phase[2][i-1]<=0:
+        e_a,e_b,e_c = back_emf_phase(theta_e_2pi,w[i-1],ke)
+        V_ab = V
+        V_bc = 0.5*(-V + e_a + e_b - 2*e_c)
 
-    elif theta_e_2pi >= 2*np.pi/3. and theta_e_2pi < 3*np.pi/3.:
-        i_phase[0].append(0.0)
-        i_phase[1].append((delta_t/L)*(V_seq['3'][1] - R*i_phase[1][i-1] -kb*back_emf_func(theta_e_2pi - 2*np.pi/3.)*w[i-1]) + i_phase[1][i-1])
-        i_phase[2].append((delta_t/L)*(V_seq['3'][2] - R*i_phase[2][i-1] -kb*back_emf_func(theta_e_2pi - 4*np.pi/3.)*w[i-1]) + i_phase[2][i-1])
-        back_emf_a.append(0.)
+        i_phase[0].append((delta_t/(3*L))*(2*V_ab + V_bc - 3*R*i_phase[0][i-1] - (2*e_a - e_b - e_c)) + i_phase[0][i-1])
+        i_phase[1].append((delta_t/(3*L))*(-V_ab + V_bc - 3*R*i_phase[1][i-1] - (-e_a + 2*e_b - e_c)) + i_phase[1][i-1])
+        i_phase[2].append(-i_phase[0][i]-i_phase[1][i])
 
-    elif theta_e_2pi >= 3*np.pi/3. and theta_e_2pi < 4*np.pi/3.:
-        i_phase[0].append((delta_t/L)*(V_seq['4'][0] - R*i_phase[0][i-1] -kb*back_emf_func(theta_e_2pi)*w[i-1]) + i_phase[0][i-1])
-        i_phase[1].append((delta_t/L)*(V_seq['4'][1] - R*i_phase[1][i-1] -kb*back_emf_func(theta_e_2pi - 2*np.pi/3.)*w[i-1]) + i_phase[1][i-1])
-        i_phase[2].append(0.0)
-        back_emf_a.append(kb*back_emf_func(theta_e_2pi)*w[i-1])
+    elif theta_e_2pi >= 0.0 and theta_e_2pi < np.pi/3. and i_phase[1][i-1] <=0 and i_phase[2][i-1]>0:
+        e_a,e_b,e_c = back_emf_phase(theta_e_2pi,w[i-1],ke)
+        V_ab = V
+        V_bc = 0.
 
-    elif theta_e_2pi >= 4*np.pi/3. and theta_e_2pi < 5*np.pi/3.:
-        i_phase[0].append((delta_t/L)*(V_seq['5'][0] - R*i_phase[0][i-1] -kb*back_emf_func(theta_e_2pi)*w[i-1]) + i_phase[0][i-1])
-        i_phase[1].append(0.0)
-        i_phase[2].append((delta_t/L)*(V_seq['5'][2] - R*i_phase[2][i-1] -kb*back_emf_func(theta_e_2pi- 4*np.pi/3.)*w[i-1]) + i_phase[2][i-1])
-        back_emf_a.append(kb*back_emf_func(theta_e_2pi)*w[i-1])
+        i_phase[0].append((delta_t/(3*L))*(2*V_ab + V_bc - 3*R*i_phase[0][i-1] - (2*e_a - e_b - e_c)) + i_phase[0][i-1])
+        i_phase[1].append((delta_t/(3*L))*(-V_ab + V_bc - 3*R*i_phase[1][i-1] - (-e_a + 2*e_b - e_c)) + i_phase[1][i-1])
+        i_phase[2].append(-i_phase[0][i]-i_phase[1][i])
 
-    elif theta_e_2pi >= 5*np.pi/3. and theta_e_2pi < 6*np.pi/3.:
-        i_phase[0].append(0.0)
-        i_phase[1].append((delta_t/L)*(V_seq['6'][1] - R*i_phase[1][i-1] -kb*back_emf_func(theta_e_2pi- 2*np.pi/3.)*w[i-1]) + i_phase[1][i-1])
-        i_phase[2].append((delta_t/L)*(V_seq['6'][2] - R*i_phase[2][i-1] -kb*back_emf_func(theta_e_2pi- 4*np.pi/3.)*w[i-1]) + i_phase[2][i-1])
-        back_emf_a.append(0.)
+    elif theta_e_2pi >= 0.0 and theta_e_2pi < np.pi/3. and i_phase[1][i-1]>0 and i_phase[2][i-1]>=0:
+        e_a,e_b,e_c = back_emf_phase(theta_e_2pi,w[i-1],ke)
+        V_ab = V
+        V_bc = 0.5*(-V + e_a + e_b -2*e_c)
+
+        i_phase[0].append((delta_t/(3*L))*(2*V_ab + V_bc - 3*R*i_phase[0][i-1] - (2*e_a - e_b - e_c)) + i_phase[0][i-1])
+        i_phase[1].append((delta_t/(3*L))*(-V_ab + V_bc - 3*R*i_phase[1][i-1] - (-e_a + 2*e_b - e_c)) + i_phase[1][i-1])
+        i_phase[2].append(-i_phase[0][i]-i_phase[1][i])
+
+    elif theta_e_2pi >= 0.0 and theta_e_2pi <= np.pi/3. and i_phase[1][i-1] >0 and i_phase[2][i-1]<0:
+        e_a,e_b,e_c = back_emf_phase(theta_e_2pi,w[i-1],ke)
+        V_ab = V
+        V_bc = -V
+
+        i_phase[0].append((delta_t/(3*L))*(2*V_ab + V_bc - 3*R*i_phase[0][i-1] - (2*e_a - e_b - e_c)) + i_phase[0][i-1])
+        i_phase[1].append((delta_t/(3*L))*(-V_ab + V_bc - 3*R*i_phase[1][i-1] - (-e_a + 2*e_b - e_c)) + i_phase[1][i-1])
+        i_phase[2].append(-i_phase[0][i]-i_phase[1][i])
+
+################### 60 - 120 #########################
+    elif theta_e_2pi >= np.pi/3. and theta_e_2pi < 2*np.pi/3. and i_phase[0][i-1] >=0 and i_phase[1][i-1]>=0:
+        e_a,e_b,e_c = back_emf_phase(theta_e_2pi,w[i-1],ke)
+
+        V_ab = 0.5*(V + e_a - 2*e_b +e_c)
+        V_bc = 0.5*(V - e_a + 2*e_b - e_c)
+
+        i_phase[0].append((delta_t/(3*L))*(2*V_ab + V_bc - 3*R*i_phase[0][i-1] - (2*e_a - e_b - e_c)) + i_phase[0][i-1])
+        i_phase[1].append((delta_t/(3*L))*(-V_ab + V_bc - 3*R*i_phase[1][i-1] - (-e_a + 2*e_b - e_c)) + i_phase[1][i-1])
+        i_phase[2].append(-i_phase[0][i]-i_phase[1][i])
+
+    elif theta_e_2pi >= np.pi/3. and theta_e_2pi < 2*np.pi/3. and i_phase[0][i-1] >=0 and i_phase[1][i-1]<0:
+        e_a,e_b,e_c = back_emf_phase(theta_e_2pi,w[i-1],ke)
+        V_ab = 0.
+        V_bc = V
+
+        i_phase[0].append((delta_t/(3*L))*(2*V_ab + V_bc - 3*R*i_phase[0][i-1] - (2*e_a - e_b - e_c)) + i_phase[0][i-1])
+        i_phase[1].append((delta_t/(3*L))*(-V_ab + V_bc - 3*R*i_phase[1][i-1] - (-e_a + 2*e_b - e_c)) + i_phase[1][i-1])
+        i_phase[2].append(-i_phase[0][i]-i_phase[1][i])
+
+    elif theta_e_2pi >= np.pi/3. and theta_e_2pi < 2*np.pi/3. and i_phase[0][i-1]<0 and i_phase[1][i-1]<=0:
+        e_a,e_b,e_c = back_emf_phase(theta_e_2pi,w[i-1],ke)
+        V_ab = 0.5*(V + e_a - 2*e_b +e_c)
+        V_bc = 0.5*(V - e_a + 2*e_b -e_c)
+
+        i_phase[0].append((delta_t/(3*L))*(2*V_ab + V_bc - 3*R*i_phase[0][i-1] - (2*e_a - e_b - e_c)) + i_phase[0][i-1])
+        i_phase[1].append((delta_t/(3*L))*(-V_ab + V_bc - 3*R*i_phase[1][i-1] - (-e_a + 2*e_b - e_c)) + i_phase[1][i-1])
+        i_phase[2].append(-i_phase[0][i]-i_phase[1][i])
+    elif theta_e_2pi >= np.pi/3. and theta_e_2pi <= 2*np.pi/3. and i_phase[0][i-1] <0 and i_phase[1][i-1]>0:
+        e_a,e_b,e_c = back_emf_phase(theta_e_2pi,w[i-1],ke)
+        V_ab = V
+        V_bc = 0.
+
+        i_phase[0].append((delta_t/(3*L))*(2*V_ab + V_bc - 3*R*i_phase[0][i-1] - (2*e_a - e_b - e_c)) + i_phase[0][i-1])
+        i_phase[1].append((delta_t/(3*L))*(-V_ab + V_bc - 3*R*i_phase[1][i-1] - (-e_a + 2*e_b - e_c)) + i_phase[1][i-1])
+        i_phase[2].append(-i_phase[0][i]-i_phase[1][i])
+
+################### 120 - 180 #########################s
+    elif theta_e_2pi >= 2*np.pi/3. and theta_e_2pi < 3*np.pi/3. and i_phase[0][i-1] <=0 and i_phase[2][i-1]<=0:
+        e_a,e_b,e_c = back_emf_phase(theta_e_2pi,w[i-1],ke)
+
+        V_ab = 0.5*(-V + 2*e_a - e_b - e_c)
+        V_bc = V
+
+        i_phase[0].append((delta_t/(3*L))*(2*V_ab + V_bc - 3*R*i_phase[0][i-1] - (2*e_a - e_b - e_c)) + i_phase[0][i-1])
+        i_phase[1].append((delta_t/(3*L))*(-V_ab + V_bc - 3*R*i_phase[1][i-1] - (-e_a + 2*e_b - e_c)) + i_phase[1][i-1])
+        i_phase[2].append(-i_phase[0][i]-i_phase[1][i])
+
+    elif theta_e_2pi >= 2*np.pi/3. and theta_e_2pi < 3*np.pi/3. and i_phase[0][i-1] >0 and i_phase[2][i-1]<=0:
+        e_a,e_b,e_c = back_emf_phase(theta_e_2pi,w[i-1],ke)
+        V_ab = -V
+        V_bc = V
+
+        i_phase[0].append((delta_t/(3*L))*(2*V_ab + V_bc - 3*R*i_phase[0][i-1] - (2*e_a - e_b - e_c)) + i_phase[0][i-1])
+        i_phase[1].append((delta_t/(3*L))*(-V_ab + V_bc - 3*R*i_phase[1][i-1] - (-e_a + 2*e_b - e_c)) + i_phase[1][i-1])
+        i_phase[2].append(-i_phase[0][i]-i_phase[1][i])
+
+    elif theta_e_2pi >= 2*np.pi/3. and theta_e_2pi < 3*np.pi/3. and i_phase[0][i-1]>=0 and i_phase[2][i-1]>0:
+        e_a,e_b,e_c = back_emf_phase(theta_e_2pi,w[i-1],ke)
+        V_ab = 0.5*(-V+2*e_a - e_b - e_c)
+        V_bc = V
+
+        i_phase[0].append((delta_t/(3*L))*(2*V_ab + V_bc - 3*R*i_phase[0][i-1] - (2*e_a - e_b - e_c)) + i_phase[0][i-1])
+        i_phase[1].append((delta_t/(3*L))*(-V_ab + V_bc - 3*R*i_phase[1][i-1] - (-e_a + 2*e_b - e_c)) + i_phase[1][i-1])
+        i_phase[2].append(-i_phase[0][i]-i_phase[1][i])
+
+    elif theta_e_2pi >= 2*np.pi/3. and theta_e_2pi <= 3*np.pi/3. and i_phase[0][i-1] <0 and i_phase[2][i-1]>0:
+        e_a,e_b,e_c = back_emf_phase(theta_e_2pi,w[i-1],ke)
+        V_ab = 0.
+        V_bc = V
+
+        i_phase[0].append((delta_t/(3*L))*(2*V_ab + V_bc - 3*R*i_phase[0][i-1] - (2*e_a - e_b - e_c)) + i_phase[0][i-1])
+        i_phase[1].append((delta_t/(3*L))*(-V_ab + V_bc - 3*R*i_phase[1][i-1] - (-e_a + 2*e_b - e_c)) + i_phase[1][i-1])
+        i_phase[2].append(-i_phase[0][i]-i_phase[1][i])
+
+################### 180 - 240 #########################
+    elif theta_e_2pi >= 3*np.pi/3. and theta_e_2pi < 4*np.pi/3. and i_phase[1][i-1] >=0 and i_phase[2][i-1]>=0:
+        e_a,e_b,e_c = back_emf_phase(theta_e_2pi,w[i-1],ke)
+
+        V_ab =-V
+        V_bc = 0.5*(V +e_a + e_b -2*e_c)
+
+        i_phase[0].append((delta_t/(3*L))*(2*V_ab + V_bc - 3*R*i_phase[0][i-1] - (2*e_a - e_b - e_c)) + i_phase[0][i-1])
+        i_phase[1].append((delta_t/(3*L))*(-V_ab + V_bc - 3*R*i_phase[1][i-1] - (-e_a + 2*e_b - e_c)) + i_phase[1][i-1])
+        i_phase[2].append(-i_phase[0][i]-i_phase[1][i])
+
+    elif theta_e_2pi >= 3*np.pi/3. and theta_e_2pi < 4*np.pi/3. and i_phase[1][i-1] >=0 and i_phase[2][i-1]<0:
+        e_a,e_b,e_c = back_emf_phase(theta_e_2pi,w[i-1],ke)
+        V_ab = -V
+        V_bc = 0.
+
+        i_phase[0].append((delta_t/(3*L))*(2*V_ab + V_bc - 3*R*i_phase[0][i-1] - (2*e_a - e_b - e_c)) + i_phase[0][i-1])
+        i_phase[1].append((delta_t/(3*L))*(-V_ab + V_bc - 3*R*i_phase[1][i-1] - (-e_a + 2*e_b - e_c)) + i_phase[1][i-1])
+        i_phase[2].append(-i_phase[0][i]-i_phase[1][i])
+
+    elif theta_e_2pi >= 3*np.pi/3. and theta_e_2pi < 4*np.pi/3. and i_phase[1][i-1]<0 and i_phase[2][i-1]<=0:
+        e_a,e_b,e_c = back_emf_phase(theta_e_2pi,w[i-1],ke)
+        V_ab = -V
+        V_bc = 0.5*(V + e_a + e_b - 2*e_c)
+
+        i_phase[0].append((delta_t/(3*L))*(2*V_ab + V_bc - 3*R*i_phase[0][i-1] - (2*e_a - e_b - e_c)) + i_phase[0][i-1])
+        i_phase[1].append((delta_t/(3*L))*(-V_ab + V_bc - 3*R*i_phase[1][i-1] - (-e_a + 2*e_b - e_c)) + i_phase[1][i-1])
+        i_phase[2].append(-i_phase[0][i]-i_phase[1][i])
+
+    elif theta_e_2pi >= 3*np.pi/3. and theta_e_2pi <= 4*np.pi/3. and i_phase[1][i-1] <0 and i_phase[2][i-1]>0:
+        e_a,e_b,e_c = back_emf_phase(theta_e_2pi,w[i-1],ke)
+        V_ab = -V
+        V_bc = V
+
+        i_phase[0].append((delta_t/(3*L))*(2*V_ab + V_bc - 3*R*i_phase[0][i-1] - (2*e_a - e_b - e_c)) + i_phase[0][i-1])
+        i_phase[1].append((delta_t/(3*L))*(-V_ab + V_bc - 3*R*i_phase[1][i-1] - (-e_a + 2*e_b - e_c)) + i_phase[1][i-1])
+        i_phase[2].append(-i_phase[0][i]-i_phase[1][i])
+
+################### 240 - 300 #########################
+    elif theta_e_2pi >= 4*np.pi/3. and theta_e_2pi < 5*np.pi/3. and i_phase[0][i-1] <=0 and i_phase[1][i-1] <=0:
+        e_a,e_b,e_c = back_emf_phase(theta_e_2pi,w[i-1],ke)
+
+        V_ab = 0.5*(-V +e_a - 2*e_b +e_c)
+        V_bc = 0.5*(-V -e_a + 2*e_b -e_c)
+
+        i_phase[0].append((delta_t/(3*L))*(2*V_ab + V_bc - 3*R*i_phase[0][i-1] - (2*e_a - e_b - e_c)) + i_phase[0][i-1])
+        i_phase[1].append((delta_t/(3*L))*(-V_ab + V_bc - 3*R*i_phase[1][i-1] - (-e_a + 2*e_b - e_c)) + i_phase[1][i-1])
+        i_phase[2].append(-i_phase[0][i]-i_phase[1][i])
+
+    elif theta_e_2pi >= 4*np.pi/3. and theta_e_2pi < 5*np.pi/3. and i_phase[0][i-1] <=0 and i_phase[1][i-1]>0:
+        e_a,e_b,e_c = back_emf_phase(theta_e_2pi,w[i-1],ke)
+        V_ab = 0.
+        V_bc = -V
+
+        i_phase[0].append((delta_t/(3*L))*(2*V_ab + V_bc - 3*R*i_phase[0][i-1] - (2*e_a - e_b - e_c)) + i_phase[0][i-1])
+        i_phase[1].append((delta_t/(3*L))*(-V_ab + V_bc - 3*R*i_phase[1][i-1] - (-e_a + 2*e_b - e_c)) + i_phase[1][i-1])
+        i_phase[2].append(-i_phase[0][i]-i_phase[1][i])
+
+    elif theta_e_2pi >= 4*np.pi/3. and theta_e_2pi < 5*np.pi/3. and i_phase[0][i-1]>0 and i_phase[1][i-1]>=0:
+        e_a,e_b,e_c = back_emf_phase(theta_e_2pi,w[i-1],ke)
+        V_ab = 0.5*(-V + e_a - 2*e_b + e_c)
+        V_bc = 0.5*(-V - e_a + 2*e_b - e_c)
+
+        i_phase[0].append((delta_t/(3*L))*(2*V_ab + V_bc - 3*R*i_phase[0][i-1] - (2*e_a - e_b - e_c)) + i_phase[0][i-1])
+        i_phase[1].append((delta_t/(3*L))*(-V_ab + V_bc - 3*R*i_phase[1][i-1] - (-e_a + 2*e_b - e_c)) + i_phase[1][i-1])
+        i_phase[2].append(-i_phase[0][i]-i_phase[1][i])
+
+    elif theta_e_2pi >= 4*np.pi/3. and theta_e_2pi <= 5*np.pi/3. and i_phase[0][i-1] >0 and i_phase[1][i-1]<0:
+        e_a,e_b,e_c = back_emf_phase(theta_e_2pi,w[i-1],ke)
+        V_ab = -V
+        V_bc = 0.
+
+        i_phase[0].append((delta_t/(3*L))*(2*V_ab + V_bc - 3*R*i_phase[0][i-1] - (2*e_a - e_b - e_c)) + i_phase[0][i-1])
+        i_phase[1].append((delta_t/(3*L))*(-V_ab + V_bc - 3*R*i_phase[1][i-1] - (-e_a + 2*e_b - e_c)) + i_phase[1][i-1])
+        i_phase[2].append(-i_phase[0][i]-i_phase[1][i])
+
+################### 300 - 360 #########################
+    elif theta_e_2pi >= 5*np.pi/3. and theta_e_2pi < 6*np.pi/3. and i_phase[0][i-1] >=0 and i_phase[2][i-1] >=0:
+        e_a,e_b,e_c = back_emf_phase(theta_e_2pi,w[i-1],ke)
+
+        V_ab = 0.5*(V + 2*e_a - e_b - e_c)
+        V_bc = -V
+
+        i_phase[0].append((delta_t/(3*L))*(2*V_ab + V_bc - 3*R*i_phase[0][i-1] - (2*e_a - e_b - e_c)) + i_phase[0][i-1])
+        i_phase[1].append((delta_t/(3*L))*(-V_ab + V_bc - 3*R*i_phase[1][i-1] - (-e_a + 2*e_b - e_c)) + i_phase[1][i-1])
+        i_phase[2].append(-i_phase[0][i]-i_phase[1][i])
+
+    elif theta_e_2pi >= 5*np.pi/3. and theta_e_2pi < 6*np.pi/3. and i_phase[0][i-1] <0 and i_phase[2][i-1]>=0:
+        e_a,e_b,e_c = back_emf_phase(theta_e_2pi,w[i-1],ke)
+        V_ab = V
+        V_bc = -V
+
+        i_phase[0].append((delta_t/(3*L))*(2*V_ab + V_bc - 3*R*i_phase[0][i-1] - (2*e_a - e_b - e_c)) + i_phase[0][i-1])
+        i_phase[1].append((delta_t/(3*L))*(-V_ab + V_bc - 3*R*i_phase[1][i-1] - (-e_a + 2*e_b - e_c)) + i_phase[1][i-1])
+        i_phase[2].append(-i_phase[0][i]-i_phase[1][i])
+
+    elif theta_e_2pi >= 5*np.pi/3. and theta_e_2pi < 6*np.pi/3. and i_phase[0][i-1]<=0 and i_phase[2][i-1]<0:
+        e_a,e_b,e_c = back_emf_phase(theta_e_2pi,w[i-1],ke)
+        V_ab = 0.5*(V + 2*e_a - e_b - e_c)
+        V_bc = -V
+
+        i_phase[0].append((delta_t/(3*L))*(2*V_ab + V_bc - 3*R*i_phase[0][i-1] - (2*e_a - e_b - e_c)) + i_phase[0][i-1])
+        i_phase[1].append((delta_t/(3*L))*(-V_ab + V_bc - 3*R*i_phase[1][i-1] - (-e_a + 2*e_b - e_c)) + i_phase[1][i-1])
+        i_phase[2].append(-i_phase[0][i]-i_phase[1][i])
+
+    elif theta_e_2pi >= 5*np.pi/3. and theta_e_2pi <= 6*np.pi/3. and i_phase[0][i-1] >0 and i_phase[2][i-1]<0:
+        e_a,e_b,e_c = back_emf_phase(theta_e_2pi,w[i-1],ke)
+        V_ab = 0.
+        V_bc = -V
+
+        i_phase[0].append((delta_t/(3*L))*(2*V_ab + V_bc - 3*R*i_phase[0][i-1] - (2*e_a - e_b - e_c)) + i_phase[0][i-1])
+        i_phase[1].append((delta_t/(3*L))*(-V_ab + V_bc - 3*R*i_phase[1][i-1] - (-e_a + 2*e_b - e_c)) + i_phase[1][i-1])
+        i_phase[2].append(-i_phase[0][i]-i_phase[1][i])
 
     torque_a = (kt*i_phase[0][i-1]*back_emf_func(theta_e_2pi))
     torque_b = (kt*i_phase[1][i-1]*back_emf_func(theta_e_2pi- 2*np.pi/3.))
     torque_c = (kt*i_phase[2][i-1]*back_emf_func(theta_e_2pi- 4*np.pi/3.))
 
-    torque = (torque_a + torque_b + torque_c)
+    torque = 1*(torque_a + torque_b + torque_c)
 
-    if i == 15000:
+    if i == 120000:
         T_load = 0.23e-3
 
     w.append((delta_t/I)*(torque -T_load - b*w[i-1]) + w[i-1])
@@ -107,6 +301,6 @@ for i in range(1,25000):
 import matplotlib.pyplot as plt
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
-ax1.plot(range(len(i_phase[2])), i_phase[2])
+ax1.plot(range(len(i_phase[1])), i_phase[1])
 #ax1.set_ylim([0,50000])
 plt.show()
