997b94adf2d836f56600f87970aeda723dad8bfc
Kartik17 <32801164+Kartik17@users.noreply.github.com>
2018-10-09 22:40:55 +0530

    Update dcmotor_sim.py

--- Motors/dcmotor_sim.py
+++ Motors/dcmotor_sim.py
@@ -1,48 +1,75 @@
+# Libraries
 import numpy as np
 import matplotlib.pyplot as plt
 
-V = 24
-R = 10
-L = 0.24
-b = 0.000001
-kb = 0.02
-kt = 0.02
-I = 9e-6
-delta_t = 0.001
+# Functions
 
-T_load = [0., 0.001,0.01, 0.05, 0.1]
-stall_torque = kt*(V/R)
+def check_params(V,kb,kt,R,L):
+    print('No Load Response: {}'.format(V/kb))
+    print('Stall Torque: {}'.format(kt*(V/R)))
+    print('Time Constant: {:2f}'.format(L/R))
 
-i_phase = [0.]
-w = [0.]
-theta = [0.]
-i_constant =[]
-w_constant = []
-print(stall_torque, V/kb)
-
-print('Time Constant: {:2f}'.format(L/R))
-for load in T_load:
+def dc_sim(load, N, n, delta_t, V, L, R, kb, kt, I_motor, I_load):
     i_phase = [0.]
     w = [0.]
     theta = [0.]
-    for i in range(1,500000):
+    theta_output = [0.]
+    for i in range(1,5000):
+        # Current 
         i_phase.append((delta_t/L)*(V - i_phase[i-1]*R - kb*w[i-1]) + i_phase[i-1])
         if i_phase[i]>=2.4:
             i_phase = 2.4
         elif i_phase[i]<=0:
             i_phase[i] = 0.
-        w.append((delta_t/I)*(kt*i_phase[i-1] - load - b*w[i-1]) + w[i-1])
+
+        # Angular Speed
+        w.append((delta_t/(I_motor + I_load/(N**2))*(kt*i_phase[i-1] - load/(n*N) - b*w[i-1]) + w[i-1]))
         if w[i] <=0:
             w[i] = 0.
         elif w[i] >= 1200:
             w[i] = 1200.0
-        theta.append((delta_t)*w[i-1] +theta[i-1])
 
-    print(i_phase[-1], V/R)
-    print(w[-1])
-    i_constant.append(kt*i_phase[-1])
-    w_constant.append(w[-1])
-plt.plot(i_constant,w_constant)
-plt.ylim([0,1200])
-plt.xlim([0,0.05])
-plt.show()
+        # Angular Position    
+        theta.append((delta_t)*w[i-1] +theta[i-1])
+        theta_output.append(theta[i]*N)
+
+    return i_phase, w, theta_output
+
+def plot_graph(load, N, n, delta_t, V, L, R, kb, kt, I_motor, I_load):
+    i_phase, w, theta_output = dc_sim(load, N, n, delta_t, V, L, R, kb, kt, I_motor, I_load)
+    fig,ax = plt.subplots(2,2)
+    ax[0,0].plot(range(len(i_phase)), i_phase)
+    ax[0,0].set_xlabel('iteration')
+    ax[0,0].set_ylabel('Current')
+
+    ax[1,0].plot(range(len(w)), w)
+    ax[1,0].set_xlabel('iteration')
+    ax[1,0].set_ylabel('Angular Speed')
+
+    ax[0,1].plot(range(len(theta_output)), theta_output)
+    ax[0,1].set_xlabel('iteration')
+    ax[0,1].set_ylabel('Angular Position')
+
+    ax[1,1].plot(w, kt*np.array(i_phase))
+    ax[1,1].set_xlabel('Angular Speed')
+    ax[1,1].set_ylabel('Torque')
+
+    plt.show()
+
+if __name__ == '__main__':
+    # Parameters
+    V = 24
+    R = 10
+    L = 0.24
+    b = 0.000001
+    kb = 0.02
+    kt = 0.02
+    I_motor = 9e-6
+    I_load = 0.
+    N = 1
+    n = 1
+    delta_t = 0.001
+    T_load = 0.
+
+    check_params(V,kb,kt,R,L)
+    plot_graph(T_load, N, n, delta_t, V, L, R, kb, kt, I_motor, I_load)
